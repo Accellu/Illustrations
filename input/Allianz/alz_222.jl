42c05d27768f645c9001d455dbffe08f1807849f
JohnSAccellu <johns@accellu.com>
2023-03-13 11:39:21 -0400

    Fix: Function Header

--- input/Allianz/alz_222.jl
+++ input/Allianz/alz_222.jl
@@ -2,14 +2,13 @@ using JSON3, JSON, CSV, DataFrames
 
 alz_222 = copy(JSON3.read(JSON.json(CSV.File("input/Allianz/Alz_222.csv") |> DataFrame)))
 
-gf = Dict(:Growth => :alzmult)
-yf = Dict(:Income => :alzmult)
+gf = Dict(:Growth_Function => :alzmult)
+yf = Dict(:Income_Function => :alzmult)
 f = Dict(:Fee => 0.0)
 s = zeros(15) .+ 1.5
 s[1] = 1.35
 s = Dict(:Rollup => s)
 w = Dict(:W => 0.05)
-age = Dict(:Age => 60)
 
 alz_222 = Dict(:alz_222 => merge(alz_222, gf, yf, f, s, w, age))
 
