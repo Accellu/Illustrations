561088c631615c5c148d5af7d90aeb526e68d8f4
JohnSAccellu <johns@accellu.com>
2023-03-13 12:18:03 -0400

    Fix:name

--- input/Allianz/alz_360_level.jl
+++ input/Allianz/alz_360_level.jl
@@ -1,17 +1,17 @@
 using JSON3, JSON, CSV, DataFrames
 
-alz_360_level = copy(JSON3.read(JSON.json(CSV.File("input/Allianz/Alz_360_Level.csv") |> DataFrame)))
+prd = copy(JSON3.read(JSON.json(CSV.File("input/Allianz/Alz_360_Level.csv") |> DataFrame)))
 
 gf = Dict(:Growth_Function => :alzmult)
 yf = Dict(:Income_Function => :levl)
 f = Dict(:Fee => 0.013)
-s = zeros(length(alz_222[:Income]))
+s = zeros(length(prd[:Income]))
 s[1:5] .+= 1.5
 s[6:end] .+= 1.0
 s = Dict(:Rollup => s)
 w = Dict(:W => 0.061)
 
-alz_360_level = Dict(:alz_360_level => merge(alz_360_level, gf, yf, f, s, w))
+alz_360_level = Dict(:alz_360_level => merge(prd, gf, yf, f, s, w))
 
 open("output/alz_360_level.json", "w") do io
     JSON3.pretty(io, alz_360_level)
--- input/Allianz/alz_360_rising.jl
+++ input/Allianz/alz_360_rising.jl
@@ -1,17 +1,17 @@
 using JSON3, JSON, CSV, DataFrames
 
-alz_360_rising = copy(JSON3.read(JSON.json(CSV.File("input/Allianz/Alz_360_Rising.csv") |> DataFrame)))
+prd = copy(JSON3.read(JSON.json(CSV.File("input/Allianz/Alz_360_Rising.csv") |> DataFrame)))
 
 gf = Dict(:Growth_Function => :alzmult)
 yf = Dict(:Income_Function => :alzretn)
 f = Dict(:Fee => 0.013)
-s = zeros(length(alz_222[:Income]))
+s = zeros(length(prd[:Income]))
 s[1:5] .+= 1.5
 s[6:end] .+= 1.0
 s = Dict(:Rollup => s)
 w = Dict(:W => 0.056)
 
-alz_360_rising = Dict(:alz_360_level => merge(alz_360_rising, gf, yf, f, s, w))
+alz_360_rising = Dict(:alz_360_rising => merge(prd, gf, yf, f, s, w))
 
 open("output/alz_360_rising.json", "w") do io
     JSON3.pretty(io, alz_360_rising)
