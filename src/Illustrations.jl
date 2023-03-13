e52483f15c12f95183ecd02b522dce1e69d0d834
JohnSAccellu <johns@accellu.com>
2023-03-13 14:37:36 -0400

    Fix: Vec to Dict for alzprods

--- src/Illustrations.jl
+++ src/Illustrations.jl
@@ -2,7 +2,7 @@ module Illustrations
 
 using Glob, JSON3
 
-alzprods = include.(replace.(readdir(glob"*.jl", "src/inputs/Allianz"), r"src/" => ""))
+alzprods = reduce(merge, include.(replace.(readdir(glob"*.jl", "src/inputs/Allianz"), r"src/" => "")))
 
 open("src/outputs/alzprods.json", "w") do io
     JSON3.pretty(io, alzprods)
