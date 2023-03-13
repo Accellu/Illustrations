c8245ac7e5178d9c299694282b5e1c0c24adc214
JohnSAccellu <johns@accellu.com>
2023-03-13 11:51:11 -0400

    Fix: Rollup length

--- input/Allianz/alz_222.jl
+++ input/Allianz/alz_222.jl
@@ -5,7 +5,7 @@ alz_222 = copy(JSON3.read(JSON.json(CSV.File("input/Allianz/Alz_222.csv") |> Dat
 gf = Dict(:Growth_Function => :alzmult)
 yf = Dict(:Income_Function => :alzmult)
 f = Dict(:Fee => 0.0)
-s = zeros(15) .+ 1.5
+s = zeros(length(alz_222[:Income])) .+ 1.5
 s[1] = 1.35
 s = Dict(:Rollup => s)
 w = Dict(:W => 0.05)
