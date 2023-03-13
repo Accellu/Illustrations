a916e2fa7412c4eb1dee22a5fd29271214c77b5f
JohnSAccellu <johns@accellu.com>
2023-03-13 08:40:29 -0400

    Ft: Dict nams, JNL

--- src/Illustrations.jl
+++ src/Illustrations.jl
@@ -3,8 +3,9 @@ module Illustrations
 using CSV, DataFrames, JSON3, JSON
 
 ills = copy.(JSON3.read.(JSON.json.(CSV.File.("input/" .* readdir("input")) .|> DataFrame)))
+illnams = replace.(readdir("input"), ".csv" => "")
 
-ills[10]
+ills = Dict(illnams .=> ills)
 
 open("output/illustrations.json", "w") do io
     JSON3.pretty(io, ills)
