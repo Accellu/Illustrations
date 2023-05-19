23e54fbed31a986a933368d492cc344401d7cc24
j-h-stevens <115172566+j-h-stevens@users.noreply.github.com>
2023-05-19 14:07:47 -0400

    Fix: Add Buffer

--- src/inputs/Allianz/Alz_IAI_perf_rising.jl
+++ src/inputs/Allianz/Alz_IAI_perf_rising.jl
@@ -9,5 +9,6 @@ f = Dict(:Fee => 0.0195)
 s = zeros(length(prd[:Income]))
 s = Dict(:Rollup => s)
 w = Dict(:W => 0.0615)
+buff = Dict(:Buffer => 0.1)
 
-Alz_IAI_Perf_Rising = Dict(:Alz_IAI_Perf_Rising => merge(prd, gf, yf, f, s, w, rf))
+Alz_IAI_Perf_Rising = Dict(:Alz_IAI_Perf_Rising => merge(prd, gf, yf, f, s, w, rf, buff))
