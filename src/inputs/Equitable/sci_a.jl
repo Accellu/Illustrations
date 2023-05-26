63f621f557fe260fb0cd883ff176de9715bc2f16
j-h-stevens <115172566+j-h-stevens@users.noreply.github.com>
2023-05-26 09:53:51 -0400

    Fix Rollup SCISI

--- src/inputs/Equitable/sci_a.jl
+++ src/inputs/Equitable/sci_a.jl
@@ -6,7 +6,7 @@ gf = Dict(:Growth_Function => :simple_boost)
 yf = Dict(:Income_Function => :drop)
 rf = Dict(:Return_Function => :cap_buff)
 f = Dict(:Fees => Dict([:account, :rider] .=> [0.0069, 0.015]))
-s = Dict(:Rollup => zeros(length(prd[:Rollup])))
+s = Dict(:Rollup => prd[:Rollup])
 w = Dict(:W => 0.05)
 
 sci_a = Dict(:sci_a => merge(prd, gf, yf, f, s, w, rf))
--- src/inputs/Equitable/sci_l.jl
+++ src/inputs/Equitable/sci_l.jl
@@ -6,7 +6,7 @@ gf = Dict(:Growth_Function => :simple_boost)
 yf = Dict(:Income_Function => :level)
 rf = Dict(:Return_Function => :cap_buff)
 f = Dict(:Fees => Dict([:account, :rider] .=> [0.0069, 0.015]))
-s = Dict(:Rollup => zeros(length(prd[:Rollup])))
+s = Dict(:Rollup => prd[:Rollup])
 w = Dict(:W => 0.05)
 
 sci_l = Dict(:sci_l => merge(prd, gf, yf, f, s, w, rf))
