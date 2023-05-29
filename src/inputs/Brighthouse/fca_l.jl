da60036072a547988e7012bbc33bb4296438fca1
j-h-stevens <115172566+j-h-stevens@users.noreply.github.com>
2023-05-29 08:00:12 -0400

    Update fcal and drop

--- src/inputs/Brighthouse/fca_l.jl
+++ src/inputs/Brighthouse/fca_l.jl
@@ -2,11 +2,11 @@ using JSON3, JSON, CSV, DataFrames
 
 prd = copy(JSON3.read(JSON.json(CSV.File("src/inputs/Brighthouse/Flex_Choice_Access_Drop.csv") |> DataFrame)))
 
-gf = Dict(:Growth_Function => :compound)
-yf = Dict(:Income_Function => :drop)
+gf = Dict(:Growth_Function => :bh_compound)
+yf = Dict(:Income_Function => :bh_drop)
 rf = Dict(:Return_Function => :weighted_returns)
 f = Dict(:Fees => merge(Dict(:account => 0.013), Dict(:rider => 0.0135), Dict(:allocation => 0.0051)))
 s = Dict(:Rollup => zeros(length(prd[:Account])))
 w = Dict(:W => 0.05)
 
-bh_fcal = Dict(:bh_fcal => merge(prd, gf, yf, f, s, w, rf))
+bh_fcad = Dict(:bh_fcad => merge(prd, gf, yf, f, s, w, rf))
