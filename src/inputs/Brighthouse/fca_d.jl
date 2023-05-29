c00a62ebbe629e7e8a825fb6c626eecbc5943ffe
j-h-stevens <115172566+j-h-stevens@users.noreply.github.com>
2023-05-29 09:42:39 -0400

    Fix CSV

--- src/inputs/Brighthouse/fca_d.jl
+++ src/inputs/Brighthouse/fca_d.jl
@@ -4,9 +4,9 @@ prd = copy(JSON3.read(JSON.json(CSV.File("src/inputs/Brighthouse/Flex_Choice_Acc
 
 gf = Dict(:Growth_Function => :bh_compound)
 yf = Dict(:Income_Function => :bh_level)
-#rf = Dict(:Return_Function => :weighted_returns)
+rf = Dict(:Return_Function => :weighted_returns)
 f = Dict(:Fees => merge(Dict(:account => 0.013), Dict(:rider => 0.0135), Dict(:allocation => 0.0051)))
 s = Dict(:Rollup => zeros(length(prd[:Account])))
 w = Dict(:W => 0.05)
 
-bh_fcal = Dict(:bh_fcal => merge(prd, gf, yf, f, s, w))
+bh_fcal = Dict(:bh_fcal => merge(prd, gf, yf, f, s, w, rf))
