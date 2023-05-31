b4137db07d9cc9b907d303154938993e3107f339
j-h-stevens <115172566+j-h-stevens@users.noreply.github.com>
2023-05-31 10:31:20 -0400

    Fix Equitable Growth Name

--- src/inputs/Equitable/sci_l.jl
+++ src/inputs/Equitable/sci_l.jl
@@ -2,7 +2,7 @@ using JSON3, JSON, CSV, DataFrames
 
 prd = copy(JSON3.read(JSON.json(CSV.File("src/inputs/Equitable/Structured_Capital_Income_Level.csv") |> DataFrame)))
 
-gf = Dict(:Growth_Function => :equitable_boost)
+gf = Dict(:Growth_Function => :equitable_simple)
 yf = Dict(:Income_Function => :equitable_level)
 rf = Dict(:Return_Function => :cap_buff)
 f = Dict(:Fees => Dict([:account, :rider] .=> [0.0069, 0.015]))
